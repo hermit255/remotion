ffbd5f2ef61f7f6900c7d310d6c8aad47076d4a3
hermit255 <hermit255@gmail.com>
2025-12-12 02:05:37 +0900

    ts規約を強化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
 import { config } from "@remotion/eslint-config-flat";
 
-export default config;
+export default [
+  ...config,
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-member-accessibility": "error",
+      "@typescript-eslint/no-inferrable-types": "off",
+    },
+  },
+];
