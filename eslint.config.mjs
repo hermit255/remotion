57f9987f6660f351368c1bd9ee78f187abc6dc77
hermit255 <hermit255@gmail.com>
2025-12-17 10:34:31 +0900

    基本設定を微調整

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
 import { config } from "@remotion/eslint-config-flat";
+import tseslint from 'typescript-eslint';
 
 export default [
   ...config,
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    // strict設定を拡張すると、no-explicit-anyも含まれます
+    ...tseslint.configs.strict,
     rules: {
       "@typescript-eslint/explicit-function-return-type": "error",
       "@typescript-eslint/explicit-module-boundary-types": "error",
