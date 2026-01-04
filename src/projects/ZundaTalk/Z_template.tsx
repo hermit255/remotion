cb2bab35ea53a962cc2a92dee18b91a94306e51c
hermit255 <hermit255@gmail.com>
2026-01-04 17:28:29 +0900

    テンプレートでも新しい字幕表示を採用

--- src/projects/ZundaTalk/Z_template.tsx
+++ src/projects/ZundaTalk/Z_template.tsx
@@ -5,7 +5,7 @@ import { sounds, basePath as soundPath } from "../../path/sounds";
 import { ZundaTalk } from "../../templates/ZundaTalk";
 import { ZundaTalkProps } from "../../util/schema/zundaTalkSchema";
 import { Message, Scenario } from "../../util/schema/sequenceSchema";
-import { genVoiceSequence, genSubtitleSequence, getTalks } from "../../components/Talk";
+import { genVoiceSequence, genSubtitleSequenceV2, getTalks } from "../../components/Talk";
 import { getAIScript, getYoutubeDescription } from "../../util/Description";
 
 const title = "2025mmdd";
@@ -63,7 +63,7 @@ export const ZundaMetanTalk = (_props: ZundaTalkProps): React.JSX.Element => {
         }} 
       />
       <ZundaTalk talks={talks} />
-      {talks.map((talk) => genSubtitleSequence(talk))}
+      {talks.map((talk) => genSubtitleSequenceV2(talk))}
     </AbsoluteFill>
   );
 };
